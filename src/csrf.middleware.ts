a2a70de6129612612b3e38d6304f676bdc026067
OmerHerera <omer.herera@gmail.com>
2023-06-23 19:16:15 +0300

    Exclude /graphql route from CSRF protection

--- src/csrf.middleware.ts
+++ src/csrf.middleware.ts
@@ -3,7 +3,12 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 @Injectable()
 export class CsrfMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
-    // Disable CSRF protection
+    // Exclude /graphql route from CSRF protection
+    if (req.originalUrl === '/graphql') {
+      return next();
+    }
+
+    // Disable CSRF protection for other routes
     delete req.csrfToken;
     next();
   }
